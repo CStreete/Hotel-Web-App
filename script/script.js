be44d606557d210bb267bb6168c031f1f5a05d8d
CStreete <112933415+CStreete@users.noreply.github.com>
2023-01-02 15:59:24 +0100

    update

--- script/script.js
+++ script/script.js
@@ -7,7 +7,9 @@ const hamburgerIcon = document.getElementById('hamburger-icon');
 button.addEventListener('click',function(){
     
     if(hamburgerIcon.classList.contains('fa-bars')){
+        
         hamburgerIcon.classList.remove('fa-bars');
+       
         hamburgerIcon.classList.add('fa-x');
         menu.classList.toggle("is-active");
     }
@@ -24,32 +26,39 @@ let moreText = document.getElementById('more');
 let myBtn = document.getElementById('myBtn');
 let dots = document.getElementById('dots');
 
-
    myBtn.addEventListener('click', function(){
-
-    if(dots.style.display === "none"){
+    
+   
+    if(dots.style.display === "none" && myBtn.classList.contains('fa-angle-down')){
         moreText.style.display = "none";
         dots.style.display = "inline";
+        myBtn.classList.remove('fa-angle-down');
+        myBtn.classList.add('fa-angle-down');
+        myBtn.classList.remove('fa-angle-up')
+        
         
         
        
         
+        
+    
     } else {
         moreText.style.display = "inline";
         dots.style.display = "none";
-        myBtn.classList.remove('fa-angel-down');
+        myBtn.classList.add('fa-angle-up');
+       
+        
+
+        
       
      
         
     }
-
-
-
-
-
-
    })
 
+
+
+   
 
 
 
